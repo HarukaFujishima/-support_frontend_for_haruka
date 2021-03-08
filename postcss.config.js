0940e708fcbd9fd38af220bd2f54472b0487cbf5
藤島　美夏 <>
2021-03-08 21:03:42 +0900

    ファーストコミット

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+cat ./postcss.config.js
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }),
+    require('cssnano')({
+      preset: 'default',
+    })
+  ]
+}
